b07075ee492896d6001c067f98f9b9f092f10b31
Chris Grandin <chris.grandin@gmail.com>
2019-12-04 13:06:52 -0800

    Change some settings for the Rmd and add spatial plots

---
+++ R/spatial.R
@@ -0,0 +1,29 @@
+yrs <- 2019
+# Get data with lat/longs from the database for each fishery
+ct_ft_latlon <- get_spatial_catch_sql("ft") %>%
+  filter(lubridate::year(catchdate) %in% yrs)
+ct_ss_latlon <- get_spatial_catch_sql("ss") %>%
+  filter(lubridate::year(catchdate) %in% yrs)
+ct_jv_latlon <- get_spatial_catch_sql("jv") %>%
+  filter(lubridate::year(catchdate) %in% yrs)
+
+# Merge the catch data frames that have lat/longs and make them into a spatial `sf` objects
+spct <- merge_into_spatial(ct_ft_latlon,
+                           ct_ss_latlon,
+                           ct_jv_latlon)
+spct_ft <- merge_into_spatial(ct_ft_latlon)
+spct_ss <- merge_into_spatial(ct_ss_latlon)
+spct_jv <- merge_into_spatial(ct_jv_latlon)
+
+grd_ft <- make_grid(spct_ft,
+                    cell_size = 10000,
+                    min_num_fids = 3)
+grd_ss <- make_grid(spct_ss,
+                    cell_size = 10000,
+                    min_num_fids = 3)
+# grd_jv <- make_grid(spct_jv,
+#                     cell_size = 10000,
+#                     min_num_fids = 3)
+grd_all <- make_grid(spct,
+                     cell_size = 10000,
+                     min_num_fids = 3)
