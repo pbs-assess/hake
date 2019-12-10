9a1af3604ba20a0709979b737828947d5ff32262
Chris Grandin <chris.grandin@gmail.com>
2019-12-09 17:06:10 -0800

    Add otoliths image and depths plots

--- R/spatial.R
+++ R/spatial.R
@@ -1,15 +1,24 @@
 yrs <- 2019
+depth_yrs <- 2010:2019
 # Get data with lat/longs from the database for each fishery
-ct_ft_latlon <- get_spatial_catch_sql("ft") %>%
+ct_ft_latlon <- get_spatial_catch_sql("ft")
+ct_ss_latlon <- get_spatial_catch_sql("ss")
+ct_jv_latlon <- get_spatial_catch_sql("jv")
+
+ct_depths <- bind_rows(ct_ft_latlon, ct_ss_latlon) %>%
+  filter(lubridate::year(catchdate) %in% depth_yrs)
+
+ct_ft_latlon <- ct_ft_latlon %>%
   filter(lubridate::year(catchdate) %in% yrs)
-ct_ss_latlon <- get_spatial_catch_sql("ss") %>%
+ct_ss_latlon <- ct_ss_latlon %>%
   filter(lubridate::year(catchdate) %in% yrs)
-ct_jv_latlon <- get_spatial_catch_sql("jv") %>%
+ct_jv_latlon <- ct_jv_latlon %>%
   filter(lubridate::year(catchdate) %in% yrs)
 
+
 # Merge the catch data frames that have lat/longs and make them into a spatial `sf` objects
-#spct <- merge_into_spatial(ct_ft_latlon,
-#                           ct_ss_latlon,
+spct <- merge_into_spatial(ct_ft_latlon,
+                           ct_ss_latlon)
 #                           ct_jv_latlon)
 spct_ft <- merge_into_spatial(ct_ft_latlon)
 spct_ss <- merge_into_spatial(ct_ss_latlon)
