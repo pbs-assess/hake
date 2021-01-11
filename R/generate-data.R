67f82525c92e299d00a258c26e8a3e30bc91a634
Chris Grandin <chris.grandin@gmail.com>
2021-01-11 02:31:10 -0800

    Update to match hakedata

--- R/generate-data.R
+++ R/generate-data.R
@@ -1,18 +1,6 @@
-library(hakedata)
-hake_theme <- function(){
-  theme_bw() +
-    theme(legend.box.background = element_rect(fill = alpha("white", 0.7)),
-          legend.box.margin = margin(1, 1, 1, 1, "mm"),
-          legend.key = element_blank(),
-          legend.margin = margin(),
-          legend.text.align = 1,
-          panel.grid.major = element_line(colour = "darkgrey", size = 0.2),
-          panel.grid.minor = element_line(colour = "darkgrey", size = 0.1),
-          legend.background = element_rect(fill = "transparent"),
-          #panel.spacing.x=unit(3, "lines"),
-          plot.margin = unit(c(0.1, 0.6, 0.1, 0.1), "lines"))
-}
+load_all("../hakedata")
 
+theme_set(hake_theme())
 # Read data from the CSV files extracted manually
 fetch_catch_data()
 d <- load_catch_data()
@@ -20,18 +8,37 @@ d <- load_catch_data()
 fetch_sample_data()
 d <- get_age_props()
 
-fetch_spatial_catch_data("ft")
-fetch_spatial_catch_data("ss")
-fetch_spatial_catch_data("jv")
+fetch_sample_data_gfdata()
+comm <- load_sample_data()
+wa_comm <- get_comm_wa(comm)
+
+fetch_survey_data()
+surv <- load_survey_data()
+wa_surv <- get_surv_wa(surv)
+
+wa_fn <- here::here("data", "LengthWeightAge_data.rds")
+wa_old <- readRDS(wa_fn) %>%
+  filter(!is.na(Source))
+# Remove Canadian data (except JV)
+wa <- wa_old %>%
+  filter(!Source %in% c("CAN_acoustic", "CAN_shoreside")) %>%
+  bind_rows(wa_surv, wa_comm)
+write_csv(wa, wa_fn)
+saveRDS(wa, here::here("data", "LengthWeightAge_data.rds"))
+
+fetch_spatial_catch_data("ft", overwrite_file = FALSE)
+fetch_spatial_catch_data("ss", overwrite_file = FALSE)
+fetch_spatial_catch_data("jv", overwrite_file = FALSE)
 ft_depth <- load_spatial_catch_data("ft")
 ss_depth <- load_spatial_catch_data("ss")
 jv_depth <- load_spatial_catch_data("jv")
-get_depth_by_year(ft_depth, "bottom", yrs = 2008:2019)
-get_depth_by_year(ss_depth, "bottom", yrs = 2008:2019)
-get_depth_by_year(jv_depth, "bottom", yrs = 2008:2019)
-get_depth_by_year(ft_depth, "gear", yrs = 2008:2019)
-get_depth_by_year(ss_depth, "gear", yrs = 2008:2019)
-get_depth_by_year(jv_depth, "gear", yrs = 2008:2019)
+yr <- 2020
+get_depth_by_year(ft_depth, "bottom", yrs = 2008:yr)
+get_depth_by_year(ss_depth, "bottom", yrs = 2008:yr)
+get_depth_by_year(jv_depth, "bottom", yrs = 2008:yr)
+get_depth_by_year(ft_depth, "gear", yrs = 2008:yr)
+get_depth_by_year(ss_depth, "gear", yrs = 2008:yr)
+get_depth_by_year(jv_depth, "gear", yrs = 2008:yr)
 
 # Get catches from each fishery (by day)
 ct_ft <- get_catch(d, type = "ft")
@@ -42,9 +49,9 @@ create_catch_csv_file(ct_ss, here("data", "can-ss-catch-by-month.csv"))
 create_catch_csv_file(ct_jv, here("data", "can-jv-catch-by-month.csv"))
 
 # Get data with lat/longs from the database for each fishery
-spct_ft <- conv_spatial("ft")
-spct_ss <- conv_spatial("ss")
-spct_jv <- conv_spatial("jv")
+spct_ft <- conv_spatial(ft_depth)
+spct_ss <- conv_spatial(ss_depth)
+spct_jv <- conv_spatial(jv_depth)
 
 # Merge the spatial catch data frames and make them into a spatial `sf` object
 spct <- conv_spatial(spct_ft,
--- R/spatial.R
+++ R/spatial.R
@@ -1,27 +1,29 @@
+library(lubridate)
+
 yrs <- 2019
 depth_yrs <- 2010:2019
 # Get data with lat/longs from the database for each fishery
-ct_ft_latlon <- get_spatial_catch_sql("ft")
-ct_ss_latlon <- get_spatial_catch_sql("ss")
-ct_jv_latlon <- get_spatial_catch_sql("jv")
+ft <- ct_ft_latlon <- fetch_spatial_catch_data("ft")
+ss <- ct_ss_latlon <- fetch_spatial_catch_data("ss")
+jv <- ct_jv_latlon <- fetch_spatial_catch_data("jv")
 
 ct_depths <- bind_rows(ct_ft_latlon, ct_ss_latlon) %>%
-  filter(lubridate::year(catchdate) %in% depth_yrs)
+  filter(year(catchdate) %in% depth_yrs)
 
 ct_ft_latlon <- ct_ft_latlon %>%
-  filter(lubridate::year(catchdate) %in% yrs)
+  filter(year(catchdate) %in% yrs)
 ct_ss_latlon <- ct_ss_latlon %>%
-  filter(lubridate::year(catchdate) %in% yrs)
+  filter(year(catchdate) %in% yrs)
 ct_jv_latlon <- ct_jv_latlon %>%
-  filter(lubridate::year(catchdate) %in% yrs)
+  filter(year(catchdate) %in% yrs)
 
 
 # Merge the catch data frames that have lat/longs and make them into a spatial `sf` objects
-spct <- merge_into_spatial(ct_ft_latlon,
-                           ct_ss_latlon)
+spct <- conv_spatial(ct_ft_latlon,
+                     ct_ss_latlon)
 #                           ct_jv_latlon)
-spct_ft <- merge_into_spatial(ct_ft_latlon)
-spct_ss <- merge_into_spatial(ct_ss_latlon)
+spct_ft <- conv_spatial(ct_ft_latlon)
+spct_ss <- conv_spatial(ct_ss_latlon)
 #spct_jv <- merge_into_spatial(ct_jv_latlon)
 
 grd_ft <- make_grid(spct_ft,
@@ -34,26 +36,22 @@ grd_catch_ft <- make_grid(spct_ft,
                           cell_size = 10000,
                           min_num_fids = 3,
                           data_col = vars(catch),
-                          data_fncs = funs(mean))
+                          data_fncs = mean)
 grd_sum_catch_ft <- make_grid(spct_ft,
                               cell_size = 10000,
                               min_num_fids = 3,
                               data_col = vars(catch),
-                              data_fncs = funs(sum))
+                              data_fncs = sum)
 grd_catch_ss <- make_grid(spct_ss,
                           cell_size = 10000,
                           min_num_fids = 3,
                           data_col = vars(catch),
-                          data_fncs = funs(mean))
+                          data_fncs = mean)
 grd_sum_catch_ss <- make_grid(spct_ss,
                               cell_size = 10000,
                               min_num_fids = 3,
                               data_col = vars(catch),
-                              data_fncs = funs(sum))
+                              data_fncs = sum)
 
-# grd_jv <- make_grid(spct_jv,
-#                     cell_size = 10000,
-#                     min_num_fids = 3)
-#grd_all <- make_grid(spct,
-#                     cell_size = 10000,
-#                     min_num_fids = 3)
+plot_depths(ft, depth_type = "gear", fishery_type = "ft", depth_max = 600, ylim = c(NA, 1300))
+plot_depths(ss, depth_type = "gear", fishery_type = "ss", depth_max = 600, ylim = c(NA, 550))
